551373ab4904621c26549317fb36a4bca5fa2fe2
krish <krish.mehta.3822@gmail.com>
2023-09-01 15:26:24 +0530

    css removed

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../image/CSS/newcss.css";
+// import "../image/CSS/newcss.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ChatGPT from "./ChatGPT";
